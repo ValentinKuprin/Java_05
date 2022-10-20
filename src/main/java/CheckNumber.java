8e93dff243e599d61a712277f96f4271528d3c2d
xbrookx <cricket666@yandex.ru>
2022-10-20 10:13:55 +0300

    added CheckNumberMultiple

---
+++ src/main/java/CheckNumber.java
@@ -0,0 +1,24 @@
+public class CheckNumber {
+    /** Given an integer M perform the following conditional actions:
+     * If M is multiple of 7 and 9 then return "Good Number".
+     * If M is only multiple of 9 and not of 7  then return "Bad Number"
+     * If M is only multiple of 11 then return "Poor Number"
+     * If M doesn't satisfy any of the above conditions then return "-1"
+     * Учитывая целое число M, выполните следующие условные действия:
+     * Если M кратно 7 и 9, то верните "Хорошее число".
+     * Если M кратно только 9, а не 7, то верните "Неверное число".
+     * Если M кратно только 11, то верните "Плохое число".
+     * Если M не удовлетворяет ни одному из вышеперечисленных условий, то верните "-1"*/
+
+    public String checkNumber(int m) {
+        if (m % 7 ==0 && m % 9 == 0) {
+            return "Good Number";
+        } else if (m % 9 == 0 && m % 7 != 0) {
+            return "Bad Number";
+        } else if (m % 11 == 0) {
+            return "Poor Number";
+        } else {
+            return "-1";
+        }
+    }
+}
---
+++ src/test/java/CheckNumberTest.java
@@ -0,0 +1,49 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CheckNumberTest {
+    /** Given an integer M perform the following conditional actions:
+     * If M is multiple of 7 and 9 then return "Good Number".
+     * If M is only multiple of 9 and not of 7  then return "Bad Number"
+     * If M is only multiple of 11 then return "Poor Number"
+     * If M doesn't satisfy any of the above conditions then return "-1"
+     * Учитывая целое число M, выполните следующие условные действия:
+     * Если M кратно 7 и 9, то верните "Хорошее число".
+     * Если M кратно только 9, а не 7, то верните "Неверное число".
+     * Если M кратно только 11, то верните "Плохое число".
+     * Если M не удовлетворяет ни одному из вышеперечисленных условий, то верните "-1" */
+
+    @Test
+    public void testCheckNumber_Multiple9And7() {
+        int m = 63;
+        String expectedResult = "Good Number";
+        String actualResult = new CheckNumber().checkNumber(m);
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+
+    @Test
+    public void testCheckNumber_Multiple9Not7() {
+        int m = 18;
+        String expectedResult = "Bad Number";
+        String actualResult = new CheckNumber().checkNumber(m);
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+
+    @Test
+    public void testCheckNumber_Multiple11() {
+        int m = 110;
+        String expectedResult = "Poor Number";
+        String actualResult = new CheckNumber().checkNumber(m);
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+
+    @Test
+    public void testCheckNumber_OtherNumbers() {
+        int m = -7;
+        String expectedResult = "-1";
+        String actualResult = new CheckNumber().checkNumber(m);
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+
+
+}
