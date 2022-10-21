eaf116263e3d6e23e9a039120b8b8eb7c1da48ff
xbrookx <cricket666@yandex.ru>
2022-10-21 09:29:15 +0300

    NumberOccurrences

--- src/main/java/NumberOccurrences.java
+++ src/main/java/NumberOccurrences.java
@@ -1,61 +1,52 @@
 import java.util.Arrays;
 
 public class NumberOccurrences {
-    /** Написать алгоритм NumberOccurrences,  который принимает на вход массив целых чисел,  и возвращает массив пар
-     {число, сколько раз число встречается в массиве} */
+    /**
+     * Написать алгоритм NumberOccurrences,  который принимает на вход массив целых чисел,  и возвращает массив пар
+     * {число, сколько раз число встречается в массиве}
+     */
 
-    public static int[][] numberOccurrences(int[] arr) {
+    public int[][] numberOccurrences(int[] arr) {
         if (arr.length > 0) {
-            int theSame = 1;
             int unique = 1;
             int[] sortArr = new SortArray().sortArray(arr);
-            for (int i = 0; i < sortArr.length - 1;) {
-                for (int j = i + 1; j < sortArr.length -1;) {
-                    if (sortArr[i] == sortArr[j]) {
-                        theSame++;
-                        i++;
-                        j++;
-                    }
-                    if (sortArr[i] != sortArr[j]) {
-                        unique++;
-                        i++;
-                        j++;
-                        theSame = 1;
-                    }
-                }
-
-            }
-            int[][] numOccurrences = new int[unique][2];
-            int iterator = 0;
             for (int i = 0; i < sortArr.length - 1; i++) {
                 for (int j = i + 1; j < sortArr.length - 1; j++) {
                     if (sortArr[i] == sortArr[j]) {
-                        theSame++;
-                        i++;
-                        j++;
+                        break;
                     }
                     if (sortArr[i] != sortArr[j]) {
                         unique++;
-                        i++;
-                        j++;
-                        numOccurrences[iterator][0] = sortArr[i];
-                        numOccurrences[iterator][1] = sortArr[theSame];
-                        theSame = 1;
-                    }
-                    iterator++;
-                }
-                System.out.println(Arrays.deepToString(numOccurrences));
-                return numOccurrences;
-            }
+                        break;
 
+                    }
+                }
+            }
+            int[][] numOccurrences = new int[unique][2];
+            int iterator = 0;
+            int sum = 0;
+
+            for (int i = 0; i < sortArr.length; i+= sum) {
+                numOccurrences[iterator][0] = sortArr[i];
+                sum = 0;
+
+                for (int j = 0; j < sortArr.length; j++) {
+                    if (sortArr[i] == sortArr[j]) {
+                        sum++;
+                    }
+                    numOccurrences[iterator][1] = sum;
+                }
+                iterator++;
+            }
+            System.out.println(Arrays.deepToString(numOccurrences));
+            return numOccurrences;
         }
         return new int[0][0];
     }
-
-    public static void main(String[] args) {
-
-        System.out.println(Arrays.deepToString(numberOccurrences(new int[]{3, 2, 5, 3, 1, 5, 4, 2, 1, 4, 5, 3, 2, 1, 4, 5, 3, 2, 1})));
-    }
+//    public static void main(String[] args) {
+//
+//        System.out.println(Arrays.deepToString(numberOccurrences(new int[]{3, 2, 5, 3, 1, 5, 4, 2, 1, 4, 5, 3, 2, 1, 4, 5, 3, 2, 1})));
+//    }
 }
 
 //1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 5, 5, 5, 5
--- src/test/java/NumberOccurrencesTest.java
+++ src/test/java/NumberOccurrencesTest.java
@@ -1,6 +1,15 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class NumberOccurrencesTest {
     /**  Test Data:
     //{3, 2, 5, 3, 1, 5, 4, 2, 1, 4, 5, 3, 2, 1, 4, 5, 3, 2, 1} →
     //{{1, 4}, {2, 4}, {3, 4}, {4, 3}, {5, 4}} */
-
+    @Test
+    public void testNumberOccurrences() {
+        int[] arr = {3, 2, 5, 3, 1, 5, 4, 2, 1, 4, 5, 3, 2, 1, 4, 5, 3, 2, 1};
+        int[][] expectedResult = {{1, 4}, {2, 4}, {3, 4}, {4, 3}, {5, 4}};
+        int[][] actualResult = new NumberOccurrences().numberOccurrences(arr);
+        Assert.assertEquals(actualResult, expectedResult);
+    }
 }
