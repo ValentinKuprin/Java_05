473b025e284e5f8d4a7eef6337779b4de20de02d
Valentin Kuprin <cricket666@yandex.ru>
2022-10-24 15:49:08 +0300

    createOddEvenArray

--- src/main/java/OddEvenElementsInArray.java
+++ src/main/java/OddEvenElementsInArray.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class OddEvenElementsInArray {
     /**
      * В классе OddEvenElementsInArray написать метод countOddEvenValuesInArray(), который принимает на вход массив
@@ -22,4 +24,35 @@ public class OddEvenElementsInArray {
 
         return new int[0];
     }
+
+    /** В классе OddEvenElementsInArray написать метод createOddEvenArray(), который принимает массив целых
+     * случайных чисел, и возвращает двумерный массив (массив четных и массив нечетных чисел) */
+    public int[][] createOddEvenArray(int[] arr) {
+        if (arr.length > 0) {
+            int countEven = 0;
+            int countOdd = 0;
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] % 2 == 0) {
+                    countEven++;
+                } else {
+                    countOdd++;
+                }
+            }
+            int newArr[][] = new int[countEven][countOdd];
+            int j = 0;
+            int k = 0;
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] % 2 == 0) {
+                    newArr[j][countEven] = arr[i];
+                    j++;
+                } else {
+                    newArr[countOdd][k] = arr[i];
+                    k++;
+                }
+            }
+            System.out.println(Arrays.deepEquals(newArr));
+        }
+
+    }
+
 }
