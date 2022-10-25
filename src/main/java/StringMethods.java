1ecaabcf702d30a5dee754dd6323ada305d7817c
Valentin Kuprin <cricket666@yandex.ru>
2022-10-25 15:14:18 +0300

    countJava()

--- src/main/java/StringMethods.java
+++ src/main/java/StringMethods.java
@@ -93,7 +93,8 @@ public class StringMethods {
     /** Видео2: https://www.youtube.com/watch?v=thzfsPEYXfI
      Методы
      charAt(), concat(), contains(), substring(), toLowerCase(),  toUpperCase()
-     Напишите метод countAs(), который принимает на вход строку и считает, сколько букв а или А содержится в строке. Метод возвращает количество букв a/A,  и количество букв/знаков в слове без букв a/A. Итоговый результат должен строится с помощью метода из видео 2.
+     Напишите метод countAs(), который принимает на вход строку и считает, сколько букв а или А содержится в строке.
+     Метод возвращает количество букв a/A,  и количество букв/знаков в слове без букв a/A. Итоговый результат должен строится с помощью метода из видео 2.
      Test Data:
      “Abracadabra” → “5, 6”
      “Homenum Revelio” → “0, 15”
@@ -116,6 +117,31 @@ public class StringMethods {
         }
 
         return "";
+    }
+
+    /** Напишите метод countJava(), который принимает на вход текст и проверяет, содержится ли в тексте хотя бы одно слово Java.
+     Test Data:
+     “As of March 2022, Java 18 is the latest version, while Java 17, 11 and 8 are the current long-term support (LTS) versions.
+     Oracle released the last zero-cost public update for the legacy version Java 8 LTS in January 2019 for commercial use,
+     although it will otherwise still support Java 8 with public updates for personal use indefinitely. Other vendors have begun
+     to offer zero-cost builds of OpenJDK 8 and 11 that are still receiving security and other upgrades.” → true
+
+     “99 little bugs in a code.
+     99 little bugs in a code.
+     Take one down, and patch it around.
+     235 critical bugs in the code.” → false */
+
+    public boolean countJava(String str) {
+        if (!str.isEmpty()) {
+            if (str.contains("Java")) {
+
+                return true;
+            }else{
+
+                return false;
+            }
+        }
+        return false;
     }
 
 }
--- src/test/java/StringMethodsTest.java
+++ src/test/java/StringMethodsTest.java
@@ -152,4 +152,54 @@ public class StringMethodsTest {
 
         Assert.assertEquals(actualResult, expectedResult);
     }
+
+    /**      Test Data:
+     “As of March 2022, Java 18 is the latest version, while Java 17, 11 and 8 are the current long-term support (LTS) versions.
+     Oracle released the last zero-cost public update for the legacy version Java 8 LTS in January 2019 for commercial use,
+     although it will otherwise still support Java 8 with public updates for personal use indefinitely. Other vendors have begun
+     to offer zero-cost builds of OpenJDK 8 and 11 that are still receiving security and other upgrades.” → true
+
+     “99 little bugs in a code.
+     99 little bugs in a code.
+     Take one down, and patch it around.
+     235 critical bugs in the code.” → false */
+
+    @Test
+    public void testCountJava_Contains_Java() {
+        String str = "As of March 2022, Java 18 is the latest version, while Java 17, 11 and 8 are the current long-term " +
+                "support (LTS) versions. Oracle released the last zero-cost public update for the legacy version Java 8 LTS " +
+                "in January 2019 for commercial use, although it will otherwise still support Java 8 with public updates for" +
+                " personal use indefinitely. Other vendors have begun to offer zero-cost builds of OpenJDK 8 and 11 that are" +
+                " still receiving security and other upgrades.";
+        Boolean expected = true;
+        Boolean actualResult = new StringMethods().countJava(str);
+
+        Assert.assertEquals(actualResult, expected);
+    }
+
+    @Test
+    public void testCountJava_NotContains_Java() {
+        String str = "99 little bugs in a code.\n" +
+                "     99 little bugs in a code.\n" +
+                "     Take one down, and patch it around.\n" +
+                "     235 critical bugs in the code.";
+        Boolean expected = false;
+        Boolean actualResult = new StringMethods().countJava(str);
+
+        Assert.assertEquals(actualResult, expected);
+    }
+
+    @Test
+    public void testCountJava_Contains_JavaLowerCase() {
+        String str = "As of March 2022, java 18 is the latest version, while java 17, 11 and 8 are the current long-term " +
+                "support (LTS) versions. Oracle released the last zero-cost public update for the legacy version java 8 LTS " +
+                "in January 2019 for commercial use, although it will otherwise still support java 8 with public updates for" +
+                " personal use indefinitely. Other vendors have begun to offer zero-cost builds of OpenJDK 8 and 11 that are" +
+                " still receiving security and other upgrades.";
+        Boolean expected = false;
+        Boolean actualResult = new StringMethods().countJava(str);
+
+        Assert.assertEquals(actualResult, expected);
+    }
+
 }
