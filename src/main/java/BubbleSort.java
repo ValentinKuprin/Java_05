2a12f516b42906cdeac6869b38d397f61e010cbd
Valentin Kuprin <cricket666@yandex.ru>
2022-10-31 21:19:56 +0300

    bubbleSortOfMinValue continue

--- src/main/java/BubbleSort.java
+++ src/main/java/BubbleSort.java
@@ -24,7 +24,33 @@ public class BubbleSort {
         return new int[0];
     }
 
-    public int[] sortOfMinValue(int[] array){
+    public int minValue (int[] array) {
+            int minValue = array[0];
+            int indexOfMin = 0;
+            for (int i = 1; i < array.length; i++) {
+                if (array[i] < minValue) {
+                    minValue = array[i];
+                    indexOfMin = i;
+                }
+            }
 
+            return indexOfMin;
+        }
+
+
+    public int[] sortOfMinValue(int[] array){
+        if(array != null && array.length > 0) {
+            int min = Integer.MAX_VALUE;
+            int indexOfMin = 0;
+            for (int i = 0; i <= array.length; i++) {
+                if (min > array[i]) {
+
+                        int temp = array[i];
+                        array[i] = min;
+                    }
+                }
+
+            }
+        }
     }
 }
